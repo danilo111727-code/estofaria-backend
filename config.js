0c405503d41a2729f4ee917701addc2242a4c9e9
danilo111727-code <danilo111727@gmail.com>
2026-03-22 19:45:31 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-// Configure a URL da API quando o frontend estiver no Cloudflare Pages
-// Exemplo: window.API_BASE = "https://estofaria-digital-api.onrender.com";
-window.API_BASE = "https://estofaria-digital-api.onrender.com";
+window.API_BASE = "https://estofaria-api.onrender.com";
